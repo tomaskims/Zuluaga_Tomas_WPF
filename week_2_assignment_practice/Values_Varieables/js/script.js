0e63fc35df997f4a8f736325cb15ba0940c2af57
tomaskims <tomaskims@fullsail.edu>
2013-12-07 13:57:00 -0500

    new info with variables and values

--- week_2_assignment_practice/Values_Varieables/js/script.js
+++ week_2_assignment_practice/Values_Varieables/js/script.js
@@ -1,9 +1,43 @@
+var age = "how old is tomas";
+alert(age);
+age = 24
+alert("tomas is???")
+alert(age);
+
+//assigning variables the value of other variables
+
+var string1 = "hello";
+var string2 = "goodbye";
+
+alert(string1);
+alert(string2);
+
+string2 = string1;
+
+alert(string1);
+alert(string2);
+
+string1 = "now for something different";
+
+alert(string1);
+alert(string2);
+
+console.log("assigning variables the variables with the same value of other variables");
 
 
-var yourmom = "paulina";
 
-console.log("my mother");
 
-console.log(yourmom);
+var isStudent = false; //boolean variable
 
-alert("your mom");
+var phrase = 'I don\'t know you\'all';
+
+console.log("I hope I get a good grade in this class");
+
+console.log("I got it apparently");
+
+var muppets = ["kermit","yo mama","paco"]; //array values in this order kermit=0, yo mama=1, and paco = 3
+
+console.log(muppets[2]);// gave the variable muppets a value of 0 = which is kermit in the array value before,
+                        //showing console.log kermit insteat of muppets
+
+
