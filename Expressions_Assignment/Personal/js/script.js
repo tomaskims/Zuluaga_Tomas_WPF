fd8d648152ba951c9dd2716c1c31c43299c90c00
tomaskims <tomaskims@fullsail.edu>
2013-12-07 23:07:30 -0500

    var values defined

---
+++ Expressions_Assignment/Personal/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tomaszuluaga on 12/7/13.
+ */
+//Student number 0003286681
+//Expressions Assignment
+
+var exHours = 4;
+var dayWeek = 7;
+var hourWord;
+
+hourWord = (exHours) * (dayWeek);
+
