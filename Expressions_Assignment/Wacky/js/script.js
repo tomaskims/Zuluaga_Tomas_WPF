60dfca6236d72e9d06357e74721b6528a4054a27
tomaskims <tomaskims@fullsail.edu>
2013-12-07 23:58:07 -0500

    added description and final submition

--- Expressions_Assignment/Wacky/js/script.js
+++ Expressions_Assignment/Wacky/js/script.js
@@ -1,19 +1,20 @@
 /**
  * Created by tomaszuluaga on 12/7/13.
  */
+//Student number 0003286681
+    //Assignment Expressions
+var meDogwk = 4;//The amount of days I take out my dog a week
+var momDogwk = 3;//The amount of days my mom takes out the dog a week
+var daysWeek = 7;//The amount of days in a week
 
-var meDogwk = 4;
-var momDogwk = 3;
-var daysWeek = 7;
-
-var meTimesweek = meDogwk * daysWeek;
+var meTimesweek = meDogwk * daysWeek;// The total of times I take out the dog a week
 console.log(meTimesweek);
 
-var momTimesweek = momDogwk * daysWeek;
+var momTimesweek = momDogwk * daysWeek;// The total of times my mom takes out the dog a week
 console.log(momTimesweek);
 
 // If I decide not to take out one time one day
 
-var meDogwk1;
+var meDogwk1;// The result if I miss one time of one day out of the week
 meDogwk1= meTimesweek - 1;
 console.log(meDogwk1);
