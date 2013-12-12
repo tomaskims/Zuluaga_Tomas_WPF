a18626b50ddfe40673aac5b4cd2fca5e98421b5a
tomaskims <tomaskims@fullsail.edu>
2013-12-12 18:30:43 -0500

    trying out results

--- Conditionals_worksheet/Conditionals/js/script.js
+++ Conditionals_worksheet/Conditionals/js/script.js
@@ -1,3 +1,23 @@
+
 /**
  * Created by tomaszuluaga on 12/12/13.
  */
+//Conditionals Worksheet
+
+// Group one
+
+
+
+//Givens
+
+var nextGast = 200;//miles
+var gasCap = 20;//gallons
+var gasEff = 34;// miles per gallon
+var gasTank = 10;//percent
+//if the current gas in the tank is not at the full capacity or the gas in the tank
+//if based
+if(gasTank = gasCap || gasTank >= gasEff && nextGast == gasTank){
+    console.log("Yes you will make it")
+}else{
+    console.log("No you better get gas now")
+}
