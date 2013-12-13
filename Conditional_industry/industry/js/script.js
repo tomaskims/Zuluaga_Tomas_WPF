1d45eb6e387d6e16ddc82f2ff2d686e16c744058
tomaskims <tomaskims@fullsail.edu>
2013-12-12 23:08:11 -0500

    Problem set also prompt created

--- Conditional_industry/industry/js/script.js
+++ Conditional_industry/industry/js/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by tomaszuluaga on 12/12/13.
  */
+//Conditionals Assignment
+
+// To be hired as a full time employee at my company one of the requirements is to have 2 years of experience, but
+// if you don't have that much experience, they will hire you as a part time for 6 months to train you.
+
+var newEmployee = prompt("Please enter how many years of experience you have");
+var requiremnt = 2;//years required
