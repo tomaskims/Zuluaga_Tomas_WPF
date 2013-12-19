2a728eb9c41b86193eb1f18dccb22093a47799f6
tomaskims <tomaskims@fullsail.edu>
2013-12-19 15:39:20 -0500

    provide more detail info about the amount of calories burn during a soccer game

--- Functions_Personal/Personal/js/script.js
+++ Functions_Personal/Personal/js/script.js
@@ -10,12 +10,15 @@
 
 var gamesLocalLeague = prompt("Please enter how many games you play in your local league ");
 
-function amountCalBurnt(gamesll){
+function amountCalBurnt(gamesll, minutes){
     var calPerMonth = gamesll * 600;
-    
+    var calPerMinute = calPerMonth / minutes;
     return calPerMonth;
+    return calPerMinute;
+
 }
 
 amountCalBurnt(gamesLocalLeague);
 
 console.log("you burn " + amountCalBurnt(gamesLocalLeague)+ ' calories per month if you play all of the games');
+console.log("and you burn calories " + amountCalBurnt(gamesLocalLeague)/ 90 + " per minute a month");
