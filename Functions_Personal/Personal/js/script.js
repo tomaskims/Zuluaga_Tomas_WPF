1a69c5ce2130c12c94f78a700fc26326010e9f41
tomaskims <tomaskims@fullsail.edu>
2013-12-19 15:26:14 -0500

    First attempt

--- Functions_Personal/Personal/js/script.js
+++ Functions_Personal/Personal/js/script.js
@@ -3,8 +3,19 @@
  */
 
 
+//If you burn 600 calories in a 90 minute soccer game
+
 //calories per game = 600
-//games per month = userprompt
+//games per month = user prompt
 
 var gamesLocalLeague = prompt("Please enter how many games you play in your local league ");
 
+function amountCalBurnt(gamesll){
+    var calPerMonth = gamesll * 600;
+    
+    return calPerMonth;
+}
+
+amountCalBurnt(gamesLocalLeague);
+
+console.log("you burn " + amountCalBurnt(gamesLocalLeague)+ ' calories per month if you play all of the games');
