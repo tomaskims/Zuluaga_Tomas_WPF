2f6f8a9b67e05961a7d16f4fef68407e26ba713a
tomaskims <tomaskims@fullsail.edu>
2013-12-19 17:39:20 -0500

    everything seems to work fine

--- Functions_Wacky/wacky/js/script.js
+++ Functions_Wacky/wacky/js/script.js
@@ -1,13 +1,23 @@
 /**
  * Created by tomaszuluaga on 12/19/13.
  */
+//Functions assignment wacky
+
 //a function that will return a greeting based on the time of the day
 
 function greeting(){
-    var x = ""
-    var time = new Date().getHours();
-    if (time <20)
+    var message = ""
+    var istheSunStillout = new Date().getHours();// will get the time of the day
+    if (istheSunStillout <18)//if earlier than 6 (12 + 6 = 18 ) good day
     {
-        x="good day"
+        message="Sun still out! Good day!!!"
     }
+    else{
+    message="Moon is out! Good evening"// otherwise good evening
+    }
+    console.log(message);
+
 }
+
+
+greeting();
