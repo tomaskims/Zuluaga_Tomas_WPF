b99699e4df88982cf6fa7a7e0ca5961a6baba21f
tomaskims <tomaskims@fullsail.edu>
2013-12-19 17:23:50 -0500

    star of new project is going give us a greeting based on the time of the day

---
+++ Functions_Wacky/wacky/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by tomaszuluaga on 12/19/13.
+ */
+//a function that will return a greeting based on the time of the day
+
+function greeting(){
+    var x = ""
+    var time = new Date().getHours();
+    if (time <20)
+    {
+        x="good day"
+    }
+}
