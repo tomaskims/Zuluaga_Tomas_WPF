b4b78c2609915a5da85e43752368609c0b9ab26a
tomaskims <tomaskims@fullsail.edu>
2013-12-19 21:05:18 -0500

    almost done prompts working but still missing something

--- Functions_Industry/Industry/js/script.js
+++ Functions_Industry/Industry/js/script.js
@@ -4,15 +4,26 @@
 //
 
 var priceofTickets = function(a, b, c){
-    var regularPrice = a;
-    var priceTwoppl = b;
-    var priceYoung = c;
-if(a < c || a <= c){
-    console.log(regularPrice)
+    var regularPrice = 10;
+    var priceTwoppl = 20;
+    var discountPrice = 16;
+    var lowPriceAge = 18;
+
+if(a <= lowPriceAge && a <= lowPriceAge){
+    console.log(" you will pay " + discountPrice + " for two people");
+
+}else if(a <= lowPriceAge || b <= lowPriceAge){
+    console.log(" You will pay " + lowPriceAge + "for two people");
+
+}else{
+    (a > lowPriceAge || b > lowPriceAge || c > lowPriceAge);
+    console.log("you will pay " + regularPrice + " for one person " + "and " + priceTwoppl + " if is two people and $10 for any additional person");
+ }
+
+
 
 }
 
-}
 
 
 
@@ -21,5 +32,5 @@ if(a < c || a <= c){
 
 
 
+priceofTickets(prompt("Enter your age ") || (prompt("Your friends age ") && (prompt("Anyone else's age "))));
 
-priceofTickets(10, 20, 16);
