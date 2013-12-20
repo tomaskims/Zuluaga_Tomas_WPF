dcfffaf2525941b5e10d9b993869d2a98e662a89
tomaskims <tomaskims@fullsail.edu>
2013-12-19 20:20:50 -0500

    starting all over again

--- Functions_Industry/Industry/js/script.js
+++ Functions_Industry/Industry/js/script.js
@@ -3,22 +3,23 @@
  */
 //
 
-var priceOfticket = function(x, y, age){
-    var onePerson = 19;
-    var twoPeople = 19;
-    var price = (10, 20, 16);
-    var discount = (onePerson && twoPeople );
-    if(onePerson< 18 || twoPeople <=18){
-        console.log("you pay" + price[0] + "for two tickets");
+var priceofTickets = function(a, b, c){
+    var regularPrice = a;
+    var priceTwoppl = b;
+    var priceYoung = c;
+if(a < c || a <= c){
+    console.log(regularPrice)
 
-
-    }else if(twoPeople > 18){
-        console.log("you pay " + price[1] +  " regular price");
-
-    }else{
-        console.log("whatevee"+ price[2]);
-    }
 }
 
-priceOfticket(prompt("enter your age" ));
+}
 
+
+
+
+
+
+
+
+
+priceofTickets(10, 20, 16);
